0e4d274bb5b09cc0a3e213a7d338ac6cf669737b
calebcv <manuelcarbajal519@gmail.com>
2022-06-21 20:12:33 -0500

    commit

--- Proyecto_DSW1/Controllers/HomeController.cs
+++ Proyecto_DSW1/Controllers/HomeController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Proyecto_DSW1.Models;
 
 namespace Proyecto_DSW1.Controllers
 {
     public class HomeController : Controller
     {
+        BD_DSW1Entities db = new BD_DSW1Entities();
         public ActionResult Index()
         {
-            return View();
+            return View(db.Producto.ToList().OrderBy(x => x.des_prod));
         }
 
         public ActionResult About()
