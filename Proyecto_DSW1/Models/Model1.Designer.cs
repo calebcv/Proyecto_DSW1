f711c43dbce35175b0928fdf4736ab73c1bcfe53
calebcv <manuelcarbajal519@gmail.com>
2022-06-22 13:58:43 -0500

    tablaUsuario

--- Proyecto_DSW1/Models/Model1.Designer.cs
+++ Proyecto_DSW1/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Proyecto-DSW1\Proyecto_DSW1\Proyecto_DSW1\Models\Model1.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'E:\Cibertec\DSW\ProyectoDSW\Proyecto_DSW1\Proyecto_DSW1\Models\Model1.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
